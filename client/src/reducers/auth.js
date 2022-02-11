85c13c6c5cb3c8f10994adc9abc6c05a5c86cc19
Andreas Franz <andreas.franz@ecily.com>
2022-02-11 16:35:03 +0100

    front and backend initialized

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -3,7 +3,7 @@
 // 2 Create user-reducer function
 // e.g. { type: 'LOGGED_IN_USER', payload: {name: 'Andl', role: 'Seller'}}
 //const authReducer = (state = {name:'Andl', role:'Seller'}, action) => {
-    export const authReducer = (state = {}, action) => {
+    export const authReducer = (state = {name: 'Andl'}, action) => {
         switch(action.type) {
           case "LOGGED_IN_USER":
             return { ...state, ...action.payload }
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import { readdirSync } from 'fs'
+import cors from 'cors'
+import mongoose from 'mongoose'
+const morgan = require('morgan') 
+require('dotenv').config()
+
+const app = express();
+
+//mongo connect
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
+
+  //middlewares
+app.use(cors())
+app.use(morgan('dev'))
+
+//routes
+readdirSync('./routes').map((r) => app.use('/api', require(`./routes/${r}`)))
+//normales vorgehen - für jede einzelne route: app.use('/api', router)
+
+const port = process.env.PORT || 8000
+app.listen(port, () => console.log(`Listening on port ${port}`))
